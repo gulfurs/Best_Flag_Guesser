da7e6aaedb658ff5c432c9f63bac145857d9561c
fijibottle <gulfurs@gmail.com>
2023-03-19 14:20:41 +0100

    data inserted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,63 +1,58 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './App.css';
-
-const photos = [
-  {
-    id: 1,
-    src: "https://flagpedia.net/data/flags/w580/ax.webp",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 0, // index of the correct option
-  },
-  {
-    id: 2,
-    src: "https://flagpedia.net/data/flags/w580/af.webp",
-    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-    answer: 2 // index of the correct option
-  },
-  // add more photos as needed
-];
+import { data } from './data';
 
 function App() {
-  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
-  const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
-  const handleOptionClick = (optionIndex) => {
-    setSelectedOptionIndex(optionIndex);
-    setIsAnswerCorrect(optionIndex === photos[currentPhotoIndex].answer);
+  // Randomize the order of the photos and answer options
+  const shuffledData = data.sort(() => Math.random() - 0.5);
+  const shuffledAnswers = shuffledData[currentQuestion].answers.sort(() => Math.random() - 0.5);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < shuffledData.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
   };
 
-  const handleNextPhotoClick = () => {
-    setCurrentPhotoIndex(currentPhotoIndex + 1);
-    setSelectedOptionIndex(null);
-    setIsAnswerCorrect(null);
+  const handleRestartButtonClick = () => {
+    setCurrentQuestion(0);
+    setShowScore(false);
+    setScore(0);
   };
-
-  const currentPhoto = photos[currentPhotoIndex];
 
   return (
     <div className="container">
-      <img src={currentPhoto.src} alt="Photo" />
-      <ul>
-        {currentPhoto.options.map((option, index) => (
-          <li
-            key={index}
-            onClick={() => handleOptionClick(index)}
-            style={{
-              cursor: "pointer",
-              fontWeight: selectedOptionIndex === index && "bold",
-              color:
-                isAnswerCorrect !== null &&
-                (isAnswerCorrect ? "green" : "red")
-            }}
-          >
-            {option}
-          </li>
-        ))}
-      </ul>
-      <button disabled={selectedOptionIndex === null} onClick={handleNextPhotoClick}>
-        Next Photo
-      </button>
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {shuffledData.length}
+          <button onClick={handleRestartButtonClick}>Restart</button>
+        </div>
+      ) : (
+        <>
+          <div className="image-container">
+            <img src={shuffledData[currentQuestion].image} alt="Question" />
+          </div>
+          <div className="answers-container">
+            <ul>
+              {shuffledAnswers.map((answer, index) => (
+                <li key={index} onClick={() => handleAnswerButtonClick(answer.isCorrect)}>
+                  {answer.text}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
