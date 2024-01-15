a532e1613482546d096604dfc673680d213fb6ba
fijibottle <gulfurs@gmail.com>
2024-01-15 23:21:05 +0100

    Carousel fail(for now) and design update

--- my-app/src/Menu.js
+++ my-app/src/Menu.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import './Menu.css';
+import Carousel from './Carousel';
 
 
 function StartMenu({ onSelectDifficulty }) {
     return (
         <div className="start-menu">
-            <h1>Welcome to FLAG_GUESSER</h1>
+            <h1>Flag Frenzy Quiz</h1>
+            {/* <img src="my-app\src\AUS.png" alt="AUS" /> */}
+            {/* <Carousel /> */}
             <h2>Select Difficulty:</h2>
-            <button onClick={() => onSelectDifficulty('easy/normal')}>Easy/Normal</button>
-            <button onClick={() => onSelectDifficulty('hard')}>Hard</button>
-            <button onClick={() => onSelectDifficulty('extreme')}>Extreme</button>
-            <button onClick={() => onSelectDifficulty('impossible')}>Impossible</button>
+            <div className='select-diff'>
+                <button onClick={() => onSelectDifficulty('easy/normal')}>Easy/Normal</button>
+                <button onClick={() => onSelectDifficulty('hard')}>Hard</button>
+                <button onClick={() => onSelectDifficulty('extreme')}>Extreme</button>
+                <button onClick={() => onSelectDifficulty('impossible')}>Impossible</button>
+            </div>
         </div>
     );
 }
