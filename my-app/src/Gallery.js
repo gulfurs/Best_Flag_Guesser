17005525ae97b9bc79753a8141a7111cf7c03865
fijibottle <gulfurs@gmail.com>
2024-06-19 22:21:45 +0200

    quick gallery fix
    To show all flags

--- my-app/src/Gallery.js
+++ my-app/src/Gallery.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import subnations from './All_subnations';
+import './App.css';
+import './Gallery.css';
 
 function Subnation_gallery() {
   return (
-    <div>
+    <div className='gallery' >
       <h1>Image display of all Sub-nations</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {subnations.map((subnation, index) => (
           <div key={index} style={{ margin: '10px', textAlign: 'center' }}>
             <img src={subnation.image} alt={`Subnation Image ${index + 1}`} style={{ width: '200px', height: '110px' }} />
@@ -18,3 +20,4 @@ function Subnation_gallery() {
 }
 
 export default Subnation_gallery;
+
